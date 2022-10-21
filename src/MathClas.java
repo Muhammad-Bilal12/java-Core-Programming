0359e59ad3b296ffdef902b528c3780aa9ece4ed
Muhammad Bilal <muhammadbilalmnaeem@gmail.com>
2022-10-21 18:54:26 +0500

    Math Class

--- src/MathClas.java
+++ src/MathClas.java
@@ -2,6 +2,16 @@ public class MathClas {
     public static void main(String[] args) {
         //Math Class
 
-        
+        // Max Number
+
+        System.out.println(Math.max(5,8)); // maximum
+        System.out.println(Math.min(5,8)); // minimum
+        // Generate Random Number between 0 and 1 in the Long Type form
+        System.out.println(Math.random());
+
+        //System.out.println((int)Math.random()); //for int Type
+        System.out.println((int)(Math.random()*100)); // for range between 0 to 100
+
+
     }
 }
